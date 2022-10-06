095657187fc2a66fffc51d446a719ee1797f1a67
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-10-06 19:02:09 -0400

    some things

--- source/Main.hx
+++ source/Main.hx
@@ -10,6 +10,7 @@ class Main extends Sprite
 	{
 		super();
 		addChild(new FPS(10, 10, 0xadffffff));
-		addChild(new FlxGame(1280, 720, MainMenuState, 1.05, 1000, 1000, true, false));
+		addChild(new FlxGame(1280, 720, MainMenuState, 1.05, 120, 120, true, false));
+		FlxG.autoPause = false;
 	}
 }
